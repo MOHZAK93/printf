16a649312c13eb7207f4b6580d159468231fbd60
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-04-07 12:34:16 +0000

    update for binary

---
+++ _str_binary.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *_str_binary - prints binary
+  *
+  *@args: function argument
+  *
+  *Return: number of bits
+  */
+
+int _str_binary(va_list args)
+{
+	int i = 0, j = 0, num, str = va_arg(args, int);
+	char *ptr;
+
+	num = str;
+	while (str)
+	{
+		str = str / 2;
+		i++;
+	}
+	ptr = malloc(sizeof(char) * i);
+
+	while (num)
+	{
+		ptr[j] = num % 2;
+		num = num / 2;
+		j++;
+	}
+
+	while (--j >= 0)
+	{
+		_putchar(ptr[j] + '0');
+	}
+
+	return (i);
+}
